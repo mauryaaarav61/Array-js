52029272ad3fdc100740af14650f2069fa038708
Rakesh Kumar <141168736+mauryaaarav61@users.noreply.github.com>
2023-10-15 01:53:26 -0700

    Add files via upload

---
+++ Array.js
@@ -0,0 +1,24 @@
+// let arr = [1,2,3,4,5,6,7,8]
+// let arr1 = ['Ram','shyam','ghanshyam','Radha']
+// // console.log(arr);
+
+// arr1.push('shayama')// add a item in last
+// arr1.pop()//remove a item from last
+// arr1.unshift('Seeta')  /// add value on 0 index
+// arr1.shift()  /// remove item from o index
+// console.log(arr1);
+
+
+
+const a ={
+    aarti:"Girl",
+    Aarav:"Boy",
+    Rajnish:"Man"
+    
+};
+
+for (const key in a) {
+   const b = key.indexOf;
+   console.log(b);
+}
+
